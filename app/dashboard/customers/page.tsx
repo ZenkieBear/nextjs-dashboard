75123eebcadc9d6f9d7a88b9be34a16afcedbe7a
Zenkie Bear <zq@zenkie.cn>
2024-05-22 15:02:07 +0800

    feat: Added customers' page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,13 @@
+import { fetchFilteredCustomers } from "@/app/lib/data"
+import Table from "@/app/ui/customers/table"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: 'Customers'
 }
-const page = () => {
-  return <p>Customers Page</p>
+const page = async () => {
+  const customers = await fetchFilteredCustomers("")
+  return <Table customers={customers} />
 }
 
 export default page
